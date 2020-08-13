d2e8700cfe86eee107bc9b7a819c197d7edfcdff
foysalious <foysalrahman112@gmail.com>
2020-08-13 12:58:52 +0600

    Emp view Controller

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -1,1 +1,18 @@
 
+const express = require('express');
+const router = express.Router();
+
+const users = require('../data');
+const loggedUser = require('../user');
+
+router.get('/', (req, res) => {
+  res.render('employee/index');
+});
+
+
+
+// router.get('/AllEmployeeList', (req, res) => {
+//   res.render('admin/allEmployee', { users });
+// });
+
+module.exports = router;
